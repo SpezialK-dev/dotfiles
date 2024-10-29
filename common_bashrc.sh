361dc772b08ee64725ba125b115788666274861e
Spezialk-dev <spezcialK@protonmail.com>
2024-10-29 11:12:22 +0100

    updated common bashrc to look better

--- common_bashrc.sh
+++ common_bashrc.sh
@@ -3,9 +3,36 @@
 # A none intrusiv bashrc extension 
 # this gets additionally loaded and does replace your normal bashrc 
 
+
+#########################################################
+#							#
+# Sources						#
+#							#
+#########################################################
+
 # Sourcing git files
 # so that we have git auto compleation on all systems regardless of configuration
 source /usr/share/git/completion/git-completion.bash
+
+
+#########################################################
+#							#
+# configurations					#
+#							#
+#########################################################
+
+# makes ist so that we can just enter a path and we will automaticly cd when tering a path
+shopt -s autocd
+
+# configuring History to only store the things we actually need which would be 
+# Stop logging consecutive identical commans 
+export HISTCONTROL=ignoredups
+
+#########################################################
+#							#
+# Aliases 						#
+#							#
+#########################################################
 
 
 #calling emacs in terminal also opens it in a terminal window 
