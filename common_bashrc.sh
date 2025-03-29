8cce5c8d071bc0de74e44d58b04d114577f8c7ff
Spezialk-dev <spezcialK@protonmail.com>
2025-03-29 15:29:56 +0100

    added new function cppcreate to create a new empty c++ project also renamed some things for ssh

--- common_bashrc.sh
+++ common_bashrc.sh
@@ -41,11 +41,57 @@ alias emacs='emacs -nw'
 #default valgrind flags
 alias vgrind="valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes --verbose"
 
-# when using kitty as a terminal it sometimes has problems with hosts that dont know it as a terminal emulator hence this fix
-alias kssh="kitten ssh"
+# when using terminals like foot or kitty that have different Term variables this can work to enable a smaller featureset without having to install the TERM variable on the host 
+alias xssh="TERM=xterm-256color ssh"
 
 # create a gitignore file that already includes the emacs files
 alias cgitignore="echo *.*~ > .gitignore"
 
 #to simply opening files with the correct thing 
 alias open="xdg-open" 
+
+
+
+#########################################################
+#							#
+# funciotns 						#
+#							#
+#########################################################
+
+# these functions are all loaded and can be called 
+
+
+# creates an entire directory optimized for c++ code, then can be simply changed to accomidate for c code 
+function cppcreate(){
+
+# checking if a name for the project is set 
+if [ -z $1 ];then  
+	echo "no name was supplyed aborting"
+	return -1
+fi
+
+
+cpppath="$(pwd)"
+
+echo "creating new project under $cpppath / $1"
+# creating all the paths 
+mkdir $cpppath/$1
+echo "created dir"
+mkdir $cpppath/$1/src
+echo "created src dir "
+
+#creating all the files 
+#makefile
+echo -e "compile:\n	 g++ -o $1 -Wall -g ./src/main.cpp" > $cpppath/$1/makefile
+echo "created makefile"
+
+# creating a hello world file 
+echo -e '#include <iostream>\n\n int main(int argc,char* argv[])\n{std::cout<<"Hello World" << std::endl; \n}' > $cpppath/$1/src/main.cpp
+
+echo "created main file "
+
+}
+
+
+
+
